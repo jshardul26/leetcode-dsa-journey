b78bd1049601f7380bbe79ef651e8c2a209c2112
jshardul26 <jshardul226@gmail.com>
2026-08-02 17:03:29 +0530

    Implement twoSum method using HashMap

--- 03-Hashing/ HashMap/TwoSum/Solution.java
+++ 03-Hashing/ HashMap/TwoSum/Solution.java
@@ -1,1 +1,20 @@
+import java.util.HashMap;
 
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int current = target - nums[i];
+
+            if (map.containsKey(current)) {
+                int j = map.get(current);
+                return new int[] {j, i};
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+
+        return new int[] {-1, -1};
+    }
+}
