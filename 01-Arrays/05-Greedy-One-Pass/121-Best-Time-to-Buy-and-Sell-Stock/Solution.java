f0bffd6792880da1cdb6b943447d05f6ede73c85
jshardul26 <jshardul226@gmail.com>
2026-08-02 19:30:46 +0530

    Implement maxProfit method for stock trading

--- 01-Arrays/05-Greedy-One-Pass/121-Best-Time-to-Buy-and-Sell-Stock/Solution.java
+++ 01-Arrays/05-Greedy-One-Pass/121-Best-Time-to-Buy-and-Sell-Stock/Solution.java
@@ -1,1 +1,21 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int profit = 0;
+        int buying = Integer.MAX_VALUE;
+        int curr_profit = 0;
+        int i = 0;
 
+        while (i < prices.length) {
+            buying = Math.min(buying, prices[i]);
+
+            if (prices[i] > buying) {
+                curr_profit = prices[i] - buying;
+            }
+
+            profit = Math.max(profit, curr_profit);
+            i++;
+        }
+
+        return profit;
+    }
+}
