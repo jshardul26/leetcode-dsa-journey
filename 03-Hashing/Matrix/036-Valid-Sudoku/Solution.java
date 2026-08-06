15949de3c423d92af40c34488c8953751005a257
Shardul Jadhav <jshardul226@gmail.com>
2026-08-06 23:31:18 +0530

    Implement Sudoku validation using HashSet

--- 03-Hashing/Matrix/036-Valid-Sudoku/Solution.java
+++ 03-Hashing/Matrix/036-Valid-Sudoku/Solution.java
@@ -1,1 +1,37 @@
+import java.util.HashSet;
 
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+
+        HashSet<String> set = new HashSet<>();
+
+        for (int row = 0; row < 9; row++) {
+
+            for (int col = 0; col < 9; col++) {
+
+                char num = board[row][col];
+
+                if (num == '.') {
+                    continue;
+                }
+
+                String rowKey = "R" + row + num;
+                String colKey = "C" + col + num;
+                String boxKey = "B" + (row / 3) + (col / 3) + num;
+
+                if (set.contains(rowKey) ||
+                    set.contains(colKey) ||
+                    set.contains(boxKey)) {
+
+                    return false;
+                }
+
+                set.add(rowKey);
+                set.add(colKey);
+                set.add(boxKey);
+            }
+        }
+
+        return true;
+    }
+}
