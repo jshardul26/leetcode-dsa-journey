6c6eec500260891812b01c1c00327b213405b28d
Shardul Jadhav <jshardul226@gmail.com>
2026-08-07 23:25:17 +0530

    Add intersection method for two arrays
    Implemented intersection method to find common elements between two arrays using HashSet.

--- 03-Hashing/HashSet/0349-intersection-of-two-arrays/Solution.java
+++ 03-Hashing/HashSet/0349-intersection-of-two-arrays/Solution.java
@@ -1,1 +1,32 @@
+import java.util.*;
 
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+
+        HashSet<Integer> set = new HashSet<>();
+
+        // Store all elements of nums1
+        for (int num : nums1) {
+            set.add(num);
+        }
+
+        HashSet<Integer> result = new HashSet<>();
+
+        // Check which elements of nums2 exist in nums1
+        for (int num : nums2) {
+            if (set.contains(num)) {
+                result.add(num);
+            }
+        }
+
+        // Convert HashSet to int[]
+        int[] answer = new int[result.size()];
+        int i = 0;
+
+        for (int num : result) {
+            answer[i++] = num;
+        }
+
+        return answer;
+    }
+}
