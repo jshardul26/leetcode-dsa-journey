d08b832582f1d3c12c1ff9ce49cdec01e7fca02b
Shardul Jadhav <jshardul226@gmail.com>
2026-08-07 23:33:10 +0530

    Update Solution.java

--- 03-Hashing/HashSet/202-happy-number/Solution.java
+++ 03-Hashing/HashSet/202-happy-number/Solution.java
@@ -1,1 +1,32 @@
+import java.util.*;
 
+class Solution {
+    public boolean isHappy(int n) {
+
+        HashSet<Integer> seen = new HashSet<>();
+
+        while (n != 1) {
+
+            // If we have already seen this number,
+            // we are stuck in a cycle.
+            if (seen.contains(n)) {
+                return false;
+            }
+
+            seen.add(n);
+
+            int sum = 0;
+
+            // Calculate sum of squares of digits
+            while (n > 0) {
+                int digit = n % 10;
+                sum += digit * digit;
+                n /= 10;
+            }
+
+            n = sum;
+        }
+
+        return true;
+    }
+}
