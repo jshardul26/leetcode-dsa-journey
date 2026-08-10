2d157d9a99cd81986c43dbf8f94e08a996f9eca5
Shardul Jadhav <jshardul226@gmail.com>
2026-08-10 23:49:44 +0530

    Update Solution.java

--- 03-Hashing/HashSet/136-single-number/Solution.java
+++ 03-Hashing/HashSet/136-single-number/Solution.java
@@ -1,1 +1,21 @@
 
+class Solution {
+    public int singleNumber(int[] nums) {
+    //    int res =0;
+    //    for(int n : nums){
+    //     res ^= n ;
+    //    }    
+    //    return res ;    
+
+    HashSet <Integer> set = new HashSet<>();
+    for(int i =0; i< nums.length;i++){
+        if(set.contains(nums[i])){
+            set.remove(nums[i]);
+        }else{
+
+        set.add(nums[i]);
+        }
+    }
+    return set.iterator().next();
+    }
+}
