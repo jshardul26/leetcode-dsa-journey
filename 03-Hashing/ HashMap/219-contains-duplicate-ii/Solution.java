c681427533b91c808588477419e1e133d4aaa0fb
Shardul Jadhav <jshardul226@gmail.com>
2026-08-11 03:43:44 +0530

    Update Solution.java

--- 03-Hashing/ HashMap/219-contains-duplicate-ii/Solution.java
+++ 03-Hashing/ HashMap/219-contains-duplicate-ii/Solution.java
@@ -1,1 +1,24 @@
+import java.util.*;
 
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int j = 0; j < nums.length; j++) {
+
+            if (map.containsKey(nums[j])) {
+
+                int i = map.get(nums[j]);
+
+                if (j - i <= k) {
+                    return true;
+                }
+            }
+
+            map.put(nums[j], j);
+        }
+
+        return false;
+    }
+}
