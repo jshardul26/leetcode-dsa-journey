abf82bfff64db334a697dc4dcca20da45f4e083e
Shardul Jadhav <jshardul226@gmail.com>
2026-08-11 04:40:34 +0530

    Update Solution.java

--- 03-Hashing/ HashMap/350-intersection-of-two-arrays-ii/Solution.java
+++ 03-Hashing/ HashMap/350-intersection-of-two-arrays-ii/Solution.java
@@ -1,1 +1,25 @@
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        int n = nums1.length;
+        int m = nums2.length;
+        
+        for(int i =0;i<n;i++){
+            map.put(nums1[i],map.getOrDefault(nums1[i],0)+1);
+        }
+        List <Integer> result = new ArrayList<>();
 
+        for(int i =0;i<m;i++){
+            if(map.containsKey(nums2[i])&& map.get(nums2[i])>0){
+                result.add(nums2[i]);
+                map.put(nums2[i],map.get(nums2[i])-1);
+                
+            }
+        }
+        int arr[] = new int[result.size()];
+        for(int i =0;i<result.size();i++){
+            arr[i]=result.get(i);
+        }
+        return arr;
+    }
+}
