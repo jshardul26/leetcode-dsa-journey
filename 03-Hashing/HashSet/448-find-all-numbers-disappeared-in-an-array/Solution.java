596e26ea7bf770b51191b7ff68af54a174e59f0d
Shardul Jadhav <jshardul226@gmail.com>
2026-08-11 02:04:09 +0530

    Add findDisappearedNumbers method in Solution class
    Implement method to find all missing numbers in an array.

--- 03-Hashing/HashSet/448-find-all-numbers-disappeared-in-an-array/Solution.java
+++ 03-Hashing/HashSet/448-find-all-numbers-disappeared-in-an-array/Solution.java
@@ -1,1 +1,25 @@
+import java.util.*;
 
+class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+
+        HashSet<Integer> set = new HashSet<>();
+        int n = nums.length;
+
+        // Store all numbers from the array
+        for (int i = 0; i < n; i++) {
+            set.add(nums[i]);
+        }
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        // Check which numbers from 1 to n are missing
+        for (int i = 1; i <= n; i++) {
+            if (!set.contains(i)) {
+                list.add(i);
+            }
+        }
+
+        return list;
+    }
+}
