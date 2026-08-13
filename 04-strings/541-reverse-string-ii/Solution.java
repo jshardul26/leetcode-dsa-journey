11bf4f055f9d1b82ce86f427e4f593c505b89b93
Shardul Jadhav <jshardul226@gmail.com>
2026-08-14 04:24:04 +0530

    Add reverseStr method to reverse string segments
    Implement a method to reverse every 2k characters in a string.

--- 04-strings/541-reverse-string-ii/Solution.java
+++ 04-strings/541-reverse-string-ii/Solution.java
@@ -1,1 +1,23 @@
+class Solution {
+    public String reverseStr(String s, int k) {
 
+        char[] ch = s.toCharArray();
+
+        for (int start = 0; start < ch.length; start += 2 * k) {
+
+            int left = start;
+            int right = Math.min(start + k - 1, ch.length - 1);
+
+            while (left < right) {
+                char temp = ch[left];
+                ch[left] = ch[right];
+                ch[right] = temp;
+
+                left++;
+                right--;
+            }
+        }
+
+        return new String(ch);
+    }
+}
