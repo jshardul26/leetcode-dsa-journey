44f44647f982cbf7a86a3002e95a9113bca4d40b
Shardul Jadhav <jshardul226@gmail.com>
2026-08-17 01:53:41 +0530

    Implement isAnagram method to check anagrams

--- 04-strings/242-valid-anagram/Solution.java
+++ 04-strings/242-valid-anagram/Solution.java
@@ -1,1 +1,26 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
 
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        int[] freq = new int[26];
+
+        // Count characters in s
+        for (int i = 0; i < s.length(); i++) {
+            freq[s.charAt(i) - 'a']++;
+        }
+
+        // Remove characters using t
+        for (int i = 0; i < t.length(); i++) {
+            freq[t.charAt(i) - 'a']--;
+
+            if (freq[t.charAt(i) - 'a'] < 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
