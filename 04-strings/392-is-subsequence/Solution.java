e25c041517e1748eb55e6414536d4f5cb196ddf6
Shardul Jadhav <jshardul226@gmail.com>
2026-08-17 00:11:11 +0530

    Implement isSubsequence method in Solution class

--- 04-strings/392-is-subsequence/Solution.java
+++ 04-strings/392-is-subsequence/Solution.java
@@ -1,1 +1,24 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int n = t.length();
+        int m = s.length();
 
+        int i = 0;
+        int j = 0;
+
+        while (i < n && j < m) {
+
+            char ch1 = t.charAt(i);
+            char ch2 = s.charAt(j);
+
+            if (ch1 == ch2) {
+                i++;
+                j++;
+            } else {
+                i++;
+            }
+        }
+
+        return j == m;
+    }
+}
