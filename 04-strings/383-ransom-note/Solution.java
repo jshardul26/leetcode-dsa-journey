7302a29d5d2e73a4caaa5e82ae100f3123c23be4
Shardul Jadhav <jshardul226@gmail.com>
2026-08-17 01:36:54 +0530

    Add frequency count for ransom note validation
    Implement frequency count to verify ransom note can be formed from magazine.

--- 04-strings/383-ransom-note/Solution.java
+++ 04-strings/383-ransom-note/Solution.java
@@ -1,1 +1,18 @@
+int[] freq = new int[26];
 
+for (int i = 0; i < magazine.length(); i++) {
+    char ch = magazine.charAt(i);
+    freq[ch - 'a']++;
+}
+
+for (int i = 0; i < ransomNote.length(); i++) {
+    char ch = ransomNote.charAt(i);
+
+    if (freq[ch - 'a'] == 0) {
+        return false;
+    }
+
+    freq[ch - 'a']--;
+}
+
+return true;
