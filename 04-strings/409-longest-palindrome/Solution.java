f2b0a740b2435f1409272a0ac68e89fb9e739b49
Shardul Jadhav <jshardul226@gmail.com>
2026-08-18 03:32:09 +0530

    Update Solution.java

--- 04-strings/409-longest-palindrome/Solution.java
+++ 04-strings/409-longest-palindrome/Solution.java
@@ -1,1 +1,29 @@
+class Solution {
+    public int longestPalindrome(String s) {
 
+        int[] freq = new int[128];
+
+        for (int i = 0; i < s.length(); i++) {
+            freq[s.charAt(i)]++;
+        }
+
+        int count = 0;
+        boolean hasOdd = false;
+
+        for (int i = 0; i < freq.length; i++) {
+
+            if (freq[i] % 2 == 0) {
+                count += freq[i];
+            } else {
+                count += freq[i] - 1;
+                hasOdd = true;
+            }
+        }
+
+        if (hasOdd) {
+            count++;
+        }
+
+        return count;
+    }
+}
