ddea5d9dac667ee2048008fc2110934b1d9d72d1
Shardul Jadhav <jshardul226@gmail.com>
2026-08-21 03:53:02 +0530

    Update Solution.java

--- 04-strings/345-reverse-vowels-of-a-string/Solution.java
+++ 04-strings/345-reverse-vowels-of-a-string/Solution.java
@@ -1,1 +1,30 @@
-
+class Solution {
+    public String reverseVowels(String s) {
+        char ch []= s.toCharArray();
+        int i =0;
+        int j = s.length()-1;
+        while(i<j){
+            if(isVowel(ch[i])&& isVowel(ch[j])){
+                char temp = ch[i];
+                ch[i]=ch[j];
+                ch[j]=temp;
+                i++;j--;
+            }else{
+                 if(!isVowel(ch[i])){
+                    i++;
+                 }else{
+                    j--;
+                 }
+            }
+        }
+        String ans = new String(ch);
+        return ans;
+    }
+    public boolean isVowel(char ch){
+        if(ch=='a'||ch=='e'|| ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U'){
+            return true;
+        }else {
+            return false;
+        }
+    }
+}
