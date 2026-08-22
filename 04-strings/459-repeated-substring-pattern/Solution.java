adf2fc8f2a24e19bc9fc8c9bee8fe913abe20821
Shardul Jadhav <jshardul226@gmail.com>
2026-08-23 04:26:44 +0530

    Update Solution.java

--- 04-strings/459-repeated-substring-pattern/Solution.java
+++ 04-strings/459-repeated-substring-pattern/Solution.java
@@ -1,1 +1,34 @@
+class Solution {
+    public boolean repeatedSubstringPattern(String s) {
 
+        int n = s.length();
+
+        // Try every possible substring length
+        for (int len = 1; len <= n / 2; len++) {
+
+            // Length must divide the total length
+            if (n % len != 0) {
+                continue;
+            }
+
+            // Candidate pattern
+            String pattern = s.substring(0, len);
+
+            StringBuilder sb = new StringBuilder();
+
+            // Repeat the pattern
+            int times = n / len;
+
+            for (int i = 0; i < times; i++) {
+                sb.append(pattern);
+            }
+
+            // Check if it matches the original string
+            if (sb.toString().equals(s)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
