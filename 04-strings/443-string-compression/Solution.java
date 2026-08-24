835d0e13bfb39efdfcea74e6d14ec3e5d9c029ac
Shardul Jadhav <jshardul226@gmail.com>
2026-08-25 03:12:49 +0530

    Update Solution.java

--- 04-strings/443-string-compression/Solution.java
+++ 04-strings/443-string-compression/Solution.java
@@ -1,1 +1,33 @@
+class Solution {
+    public int compress(char[] chars) {
 
+        int write = 0;
+        int read = 0;
+
+        while (read < chars.length) {
+
+            char current = chars[read];
+            int count = 0;
+
+            // Count consecutive occurrences
+            while (read < chars.length && chars[read] == current) {
+                read++;
+                count++;
+            }
+
+            // Write the character
+            chars[write++] = current;
+
+            // Write the count if greater than 1
+            if (count > 1) {
+                String cnt = String.valueOf(count);
+
+                for (char c : cnt.toCharArray()) {
+                    chars[write++] = c;
+                }
+            }
+        }
+
+        return write;
+    }
+}
